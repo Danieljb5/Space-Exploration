c37bb993c4f16552268e8207c399b1d78b76c57f
Danieljb5 <danieljburfoot@outlook.com>
2021-04-05 10:52:47 +0100

    test code for S2D

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include <simple2d.h>
+
+void render()
+{
+	S2D_DrawTriangle(
+        320, 50, 1, 0, 0, 1, 
+        540, 430, 0, 1, 0, 1, 
+        100, 430, 0, 0, 1, 1);
+}
 
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+	S2D_Window *window = S2D_CreateWindow("Test", 1920, 1080, NULL, render, S2D_FULLSCREEN | S2D_RESIZABLE);
+	S2D_Show(window);
 	return 0;
 }
